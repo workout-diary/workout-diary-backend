cb50e1d1bd58bc346430af4c3f4999ac60e3c1db
Tringles <jjmjm2002@naver.com>
2022-03-16 23:14:43 +0900

    chore: initialize & set up spring boot

---
+++ src/test/java/com/deukgeun/workout/WorkoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deukgeun.workout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
