ed74a6b2eccb877aab48e44702aec5e24e59d3af
Tringles <jjmjm2002@naver.com>
2022-03-19 21:58:58 +0900

    feat: OAuth2 controller & dto

---
+++ src/main/java/com/deukgeun/workout/auth/dto/AccessToken.java
@@ -0,0 +1,17 @@
+package com.deukgeun.workout.auth.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class AccessToken {
+    private String accessToken;
+}
