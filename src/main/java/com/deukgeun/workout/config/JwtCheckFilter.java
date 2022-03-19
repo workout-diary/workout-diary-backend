7a10197de4675af0365f8b46784a1563552563c6
Tringles <jjmjm2002@naver.com>
2022-03-19 21:59:38 +0900

    feat: Security & Filter
    TODO

---
+++ src/main/java/com/deukgeun/workout/config/JwtCheckFilter.java
@@ -0,0 +1,20 @@
+package com.deukgeun.workout.config;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtCheckFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+    }
+}
