9d8340fd2cce9d33f2a18837263e2ce0a8bec01a
Tringles <50797520+Tringles@users.noreply.github.com>
2022-03-19 22:31:55 +0900

    fix: Allow CORS (#2)

--- src/main/java/com/deukgeun/workout/config/SecurityConfig.java
+++ src/main/java/com/deukgeun/workout/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.deukgeun.workout.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity()
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -22,8 +26,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .cors()
+                .configurationSource(corsConfigurationSource())
                 .and()
                 .addFilterBefore(new JwtCheckFilter(), UsernamePasswordAuthenticationFilter.class)
         ;
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
