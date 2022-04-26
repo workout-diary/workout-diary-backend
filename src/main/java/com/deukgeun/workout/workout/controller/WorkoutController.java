58c7c68787d02c646878febafaec714c0219c51b
Tringles <jjmjm2002@naver.com>
2022-04-26 14:10:19 +0900

    feat: workout controller - base

---
+++ src/main/java/com/deukgeun/workout/workout/controller/WorkoutController.java
@@ -0,0 +1,11 @@
+package com.deukgeun.workout.workout.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/workout")
+public class WorkoutController {
+
+}
